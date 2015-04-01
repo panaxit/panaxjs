bdcd227d81ae4767c332277243b7c9fac6ec592f
Benjamin Orozco <benoror@gmail.com>
2015-03-31 18:06:06 -0600

    SPEC CRUD

--- spec/crud_spec.js
+++ spec/crud_spec.js
@@ -4,13 +4,59 @@
 
 var PanaxJS = require('..');
 var panax_config = require('../config/panax');
-var oPanaxJS = new PanaxJS(panax_config);
 var util = require('../lib/util');
 
+var oPanaxJS = new PanaxJS(panax_config, {
+	userId: undefined,
+	tableName: 'CatalogosSistema.Pais',
+	output: 'json'
+});
+
 describe("CRUD", function () {
-// getCatalogOptions
-// getXML
-// updateDB INSERT
-// updateDB UPDATE
-// updateDB DELETE
+
+	it("should PanaxJS.authenticate", function (done) {
+		oPanaxJS.authenticate(panax_config.ui.username, util.md5(panax_config.ui.password), function (err, userId) {
+			expect(err).toBeFalsy();
+			if(!err) {
+				expect(userId).toBeTruthy();
+				oPanaxJS.setParam("userId", userId);
+			}
+			done();
+		});
+	});
+
+	it("should PanaxJS.getCatalogOptions", function (done) {
+		var args = {
+			catalogName: "CatalogosSistema.Pais",
+			valueColumn: "Clave",
+			textColumn: "Pais"
+		};
+		oPanaxJS.getCatalogOptions(args, function (err, result) {
+			expect(err).toBeFalsy();
+			if(!err) {
+				expect(result).toBeTruthy();
+			}
+			done();
+		});
+	});
+
+	it("should PanaxJS.getXML", function (done) {
+		oPanaxJS.getXML(function (err, result) {
+			expect(err).toBeFalsy();
+			if(!err) {
+				expect(result).toBeTruthy();
+			}
+			done();
+		});
+	});
+
+	// it("should PanaxJS.updateDB INSERT", function (done) {
+	// });
+
+	// it("should PanaxJS.updateDB UPDATE", function (done) {
+	// });
+
+	// it("should PanaxJS.updateDB DELETE", function (done) {
+	// });
+
 });
