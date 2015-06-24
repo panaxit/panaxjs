af170aedb4168279e22f9780186447f4be1e56f5
Benjamin Orozco <benoror@gmail.com>
2015-06-24 17:59:41 -0500

    test(config): add placeholders

--- test/integration/config.js
+++ test/integration/config.js
@@ -1,1 +1,59 @@
-// ToDo: Config Integration Tests
+var expect = require('chai').expect;
+var PanaxJS = require('../..');
+var config = require('../../config/panax');
+var util = require('../../lib/util');
+
+describe('Config', function() {
+
+	var panaxdb = new PanaxJS(config, {
+		userId: undefined
+	});
+
+	/*
+	ToDo: DDL Isolation Stuff
+	 */
+  before('authenticate', function(done) {
+		panaxdb.authenticate(config.ui.username, util.md5(config.ui.password), function (err, userId) {
+			if(err) return done(err);
+			panaxdb.setParam("userId", userId);
+			done();
+		});
+  // CREATE Table(s)
+  // INSERT Data
+  });
+  after(function() {
+  // DROP Table(s)
+  });
+
+  // ToDo: #rebuildMetadata Tests
+  describe('#rebuildMetadata', function() {
+
+  	it('should run');
+
+  });
+
+  // ToDo: #clearCache Tests
+  describe('#clearCache', function() {
+
+  	it('should run');
+
+  });
+
+  // ToDo: #config Tests
+  describe('#config', function() {
+
+  	it('should get empty config');
+
+  	it('should set several configs');
+
+  	it('should get one config');
+
+  	it('should get all configs');
+
+  	it('should clear one config');
+
+  	it('should clear all configs');
+
+  });
+
+});
