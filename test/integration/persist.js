2552730796b05f219aac701785981835c1e32f3c
Benjamin Orozco <benoror@gmail.com>
2015-06-25 16:46:31 -0500

    test(persist): add missing test (3rd case)

--- test/integration/persist.js
+++ test/integration/persist.js
@@ -187,4 +187,97 @@ describe('Persist', function() {
   	
   });
 
+  describe('Case 3: With primaryKey & identityKey)', function() {
+
+		// ToDo: DDL Isolation Stuff
+	  // before('isolation', function(done) {
+		//  // [persist_prep.sql] CREATE Table(s)
+	  // });
+	  // after(function() {
+	  // 	// [persist_clean.sql] DROP Table(s)
+	  // });
+
+		var identityValue;
+
+		it("should insertRow", function (done) {
+			var insertXML = 
+				'<dataTable name="dbo.Empleado" identityKey="Id">' + 
+					'<insertRow>' + 
+						'<field name="RFC" isPK="true">\'\'GORU810929\'\'</field>' +
+						'<field name="Nombre">\'\'Uriel\'\'</field>' +
+						'<field name="ApellidoPaterno">\'\'Gómez\'\'</field>' +
+						'<field name="ApellidoMaterno">\'\'Robles\'\'</field>' +
+						'<field name="FechaNacimiento" out="true">NULL</field>' +
+						'<field name="FechaCaptura" out="true">NULL</field>' +
+					'</insertRow>' + 
+				'</dataTable>';
+
+			panaxdb.persist(insertXML, function (err, xml) {
+				if(err) done(err);
+				expect(xml).to.be.ok;
+				PanaxJS.Util.parseResults(xml, function (err, res) {
+					if(err) done(err);
+					expect(res).to.be.ok;
+					expect(res[0]).to.be.ok;
+					expect(res[0].status).to.equal('success');
+					expect(res[0].action).to.equal('insert');
+					expect(res[0].tableName).to.equal('[dbo].[Empleado]');
+					expect(res[0].identity).to.be.above(0);
+					identityValue = res[0].identity;
+					done();
+				});
+			});
+		});
+
+		it("should updateRow", function (done) {
+			var updateXML = 
+				'<dataTable name="dbo.Empleado" identityKey="Id">' + 
+					//'<updateRow identityValue="' + identityValue + '">' + 
+					'<updateRow>' + 
+						'<field name="RFC" isPK="true" currentValue="\'\'GORU810929\'\'">\'\'GORU8109293T0\'\'</field>' + 
+						'<field name="Nombre">\'\'Uriel Mauricio\'\'</field>' + 
+					'</updateRow>' + 
+				'</dataTable>';
+
+			panaxdb.persist(updateXML, function (err, xml) {
+				if(err) done(err);
+				expect(xml).to.be.ok;
+				PanaxJS.Util.parseResults(xml, function (err, res) {
+					if(err) done(err);
+					expect(res).to.be.ok;
+					expect(res[0]).to.be.ok;
+					expect(res[0].status).to.equal('success');
+					expect(res[0].action).to.equal('update');
+					expect(res[0].tableName).to.equal('[dbo].[Empleado]');
+					done();
+				});
+			});
+		});
+
+		it("should deleteRow", function (done) {
+			var deleteXML = 
+				'<dataTable name="dbo.Empleado" identityKey="Id">' + 
+					//'<deleteRow identityValue="' + identityValue + '">' + 
+					'<deleteRow>' + 
+						'<field name="RFC" isPK="true">\'\'GORU8109293T0\'\'</field>' + 
+					'</deleteRow>' + 
+				'</dataTable>';
+
+			panaxdb.persist(deleteXML, function (err, xml) {
+				if(err) done(err);
+				expect(xml).to.be.ok;
+				PanaxJS.Util.parseResults(xml, function (err, res) {
+					if(err) done(err);
+					expect(res).to.be.ok;
+					expect(res[0]).to.be.ok;
+					expect(res[0].status).to.equal('success');
+					expect(res[0].action).to.equal('delete');
+					expect(res[0].tableName).to.equal('[dbo].[Empleado]');
+					done();
+				});
+			});
+		});
+  	
+  });
+
 });
